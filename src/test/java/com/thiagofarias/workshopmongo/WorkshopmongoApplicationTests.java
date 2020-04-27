347e56e05b3a5bd10dee122b17527def53f3fcbf
Thiago Farias <thiagofarias99@hotmail.com>
2020-04-27 14:58:44 -0300

    created project

---
+++ src/test/java/com/thiagofarias/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagofarias.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

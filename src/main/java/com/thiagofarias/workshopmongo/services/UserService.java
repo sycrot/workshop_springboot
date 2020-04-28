c38b8cfe03c7ec49dfa9d7482abdb8b76cc4d20e
Thiago Farias <thiagofarias99@hotmail.com>
2020-04-28 10:03:56 -0300

    Getting a user by id

--- src/main/java/com/thiagofarias/workshopmongo/services/UserService.java
+++ src/main/java/com/thiagofarias/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.thiagofarias.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thiagofarias.workshopmongo.domain.User;
 import com.thiagofarias.workshopmongo.repository.UserRepository;
+import com.thiagofarias.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,14 @@ public class UserService {
 	public List<User> findAll() {
 		
 		return repo.findAll();
+		
+	}
+	
+	public User findById(String id) {
+		
+		Optional<User> user = repo.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
 	}
 }

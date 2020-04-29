7dd5d3c55d1014045d18557487d237936f5705bd
Thiago Farias <thiagofarias99@hotmail.com>
2020-04-29 15:06:36 -0300

    Simple query with @Query

--- src/main/java/com/thiagofarias/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/thiagofarias/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.thiagofarias.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.thiagofarias.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 

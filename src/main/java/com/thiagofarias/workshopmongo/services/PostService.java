e2d5ed164df78de4a8f4d4d37f5fe6a0704987b5
Thiago Farias <thiagofarias99@hotmail.com>
2020-04-29 14:57:35 -0300

    Simple query with query methods

--- src/main/java/com/thiagofarias/workshopmongo/services/PostService.java
+++ src/main/java/com/thiagofarias/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.thiagofarias.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,12 @@ public class PostService {
 		Optional<Post> user = repo.findById(id);
 		
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+	}
+	
+	public List<Post> findByTitle(String text) {
+		
+		return repo.findByTitleContainingIgnoreCase(text);
 		
 	}
 }
